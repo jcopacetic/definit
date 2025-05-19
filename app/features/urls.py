0960aa0a5f6bdab079fb21e5c05dbd52751a7996
Jonathan Sumner <jonathan@khaoticdigital.com>
2025-05-19 15:53:44 -0500

    webhook

--- app/features/urls.py
+++ app/features/urls.py
@@ -4,6 +4,6 @@ from app.features.views import hubspot_to_msgraph_webhook_listener
 
 app_name = "features"
 
-url_patterns = [
+urlpatterns = [
     path("features/feature_1/hubspot/webhook/listener/", view=hubspot_to_msgraph_webhook_listener, name="hubspot_to_ms_graph_webhook")
 ]
