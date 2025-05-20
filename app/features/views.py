5b595bd1d2c19f8b5ec2135733ddeda2c065b843
Jonathan Sumner <jonathan@khaoticdigital.com>
2025-05-19 21:24:55 -0500

    webhook

--- app/features/views.py
+++ app/features/views.py
@@ -1,9 +1,12 @@
 import logging
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 logger = logging.getLogger(__name__)
 
+
+@csrf_exempt
 def hubspot_to_msgraph_webhook_listener(request):
     logger.info(f"{vars(request)}")
     return HttpResponse(200)
