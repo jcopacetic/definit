b0f1373e6dee61e34b04a8678d3b691f8c79cf9e
Jonathan Sumner <jonathan@khaoticdigital.com>
2025-06-15 17:32:58 -0500

    row signer

--- app/ms_graph/client.py
+++ app/ms_graph/client.py
@@ -1157,7 +1157,7 @@ class MSGraphClient:
 
             # Generate signed URL
             signed_url = self._generate_signed_url(row_to_update, settings.SECRET_KEY)
-            update_link_formula = f'=HYPERLINK("{signed_url}", "update")'
+            update_link_formula = f'=HYPERLINK("https://integration00.definit.com/excel/excel-note-to-hubspot/{row_to_update}/", "update")'
 
             values = [
                 [
