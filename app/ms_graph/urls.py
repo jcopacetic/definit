7ba9a50ad4926b5fb2458a43700286b1107cada3
Jonathan Sumner <jonathan@khaoticdigital.com>
2025-06-15 17:17:55 -0500

    changes excel note update url path variable from int to slug

--- app/ms_graph/urls.py
+++ app/ms_graph/urls.py
@@ -5,5 +5,5 @@ from app.ms_graph.views import excel_note_to_hubspot
 app_name = "ms_graph"
 
 urlpatterns = [
-    path("excel-note-to-hubspot/<int:excel_row>/", view=excel_note_to_hubspot, name="excel-note-to-hubspot"),
+    path("excel-note-to-hubspot/<slug:excel_row>/", view=excel_note_to_hubspot, name="excel-note-to-hubspot"),
 ]
